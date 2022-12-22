eb18028cfd330cb41747c11e5b07e6bde837a5ed
Jack Thomson <Jackathomson@outlook.com>
2022-12-22 19:35:41 +0000

    Fix redundant move checker

--- src/board/redundant_move_filter.rs
+++ src/board/redundant_move_filter.rs
@@ -1,31 +1,30 @@
 use crate::macros::SafeGetters;
 
 const MOVE_MASK: u64 = 0b0111_1111;
-const SCORED_MASK: u64 = 1 << 63;
+const SCORED_MASK: u64 = 0b1000_0000;
 
 #[inline]
-
 pub fn check_if_previously_run(checking: usize, lookup: u64) -> bool {
-
-    let scored = lookup & SCORED_MASK;
-
-    if scored != 0 {
-        return false;
-    }
-
     let mut returning = false;
-    let pair = *AFFECTED_PAIRS.get_safely(checking);
+
+    let left = *AFFECTED_PAIRS.get_safely(checking).get_safely(0);
+    let right = *AFFECTED_PAIRS.get_safely(checking).get_safely(1);
 
     for i in 0..8 {
         let shifted = lookup >> (i * 8);
+        let scored = shifted & SCORED_MASK;        
         let move_position = shifted & MOVE_MASK;
-
-        if pair == move_position {
-            return false
-        }
-
+        
         if move_position as usize == checking {
             returning = true;
+        }
+
+        if scored > 0 {
+            returning = false;
+        }
+
+        if left == move_position || right == move_position {
+            returning = false;
         }
     }
 
@@ -35,29 +34,32 @@ pub fn check_if_previously_run(checking: usize, lookup: u64) -> bool {
 #[inline]
 pub fn record_move(total_depth: u8, curr_depth: u8, curr_mask: u64, move_loc: usize, scored: bool) -> u64 {
     let actual_depth = total_depth - curr_depth;
+    let mut move_loc = move_loc as u64;
 
     if actual_depth > 8 {
         return curr_mask;
     }
 
-    let mut res = curr_mask | ((move_loc as u64) << (actual_depth * 8));
-
     if scored {
-        res |= SCORED_MASK;
+        move_loc |= SCORED_MASK;
     }
 
-    res
+    curr_mask | (move_loc << (actual_depth * 8))
 }
 
 #[inline]
-const fn build_pair_array() -> [u64; 72] {
-    let mut end = [255; 72];
+const fn build_pair_array() -> [[u64; 2]; 72] {
+    let mut end = [[255; 2]; 72];
     let mut cntr = 0;
 
     loop {
         let x = cntr % 6;
+        if x > 0 {
+            end[cntr][0] = cntr as u64 - 1;
+        }
+
         if x < 5 {
-            end[cntr] = cntr as u64 + 1;
+            end[cntr][1] = cntr as u64 + 1;
         }
 
         cntr += 1;
@@ -69,4 +71,4 @@ const fn build_pair_array() -> [u64; 72] {
     end
 }
 
-pub const AFFECTED_PAIRS: [u64; 72] = build_pair_array();
+pub const AFFECTED_PAIRS: [[u64; 2]; 72] = build_pair_array();
